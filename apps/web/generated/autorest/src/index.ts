aae45ee63360e22ff222e0b7f09ded3d1961d5c0
HEKUCHAN <heitorhirose@gmail.com>
2025-05-20 23:33:09 +0900

    Refactor code structure for improved readability and maintainability

--- apps/web/generated/autorest/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./models/index.js";
-export { TokyoUniversityOfTechnologyBusWebAPIService } from "./tokyoUniversityOfTechnologyBusWebAPIService.js";
-export * from "./operationsInterfaces/index.js";
---
+++ apps/web/lib/client.ts
@@ -0,0 +1,8 @@
+import createClient from 'openapi-fetch';
+
+import type { paths } from '@/generated/oas';
+
+
+export const client = createClient<paths>({
+  baseUrl: `https://petstore3.swagger.io/api/v3`,
+});
